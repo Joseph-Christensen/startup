f69ea4a30c1d16e89965787ad44a5ea45468d21c
Joseph-Christensen <josephchristensen06@gmail.com>
2025-10-21 18:11:26 -0600

    weaponUtils finished

--- startup-react/src/game/weaponUtils.js
+++ startup-react/src/game/weaponUtils.js
@@ -40,6 +40,7 @@ export function compareWeapons(guess, correct) {
         traits: ''
     };
 
+    // Name
     if (isEqual(guess.name, correct.name)) {
         result.name = 'correct';
         result.category = 'correct';
@@ -47,8 +48,50 @@ export function compareWeapons(guess, correct) {
         result.damage = 'correct';
         result.armorPen = 'correct';
         result.traits = 'correct';
+        return result;
     } else {
         result.name = 'incorrect';
+    }
+
+    // Category
+    if (isEqual(guess.category, correct.category)) {
+        result.category = 'correct';
+    } else {
+        result.category = 'incorrect';
+    }
+
+    // Type
+    if (isEqual(guess.type, correct.type)) {
+        result.type = 'correct';
+    } else {    
+        result.type = 'incorrect';
+    }
+
+    // Damage
+    if (guess.damage === correct.damage) {
+        result.damage = 'correct';
+    } else if (guess.damage > correct.damage) {
+        result.damage = 'higher';
+    } else {
+        result.damage = 'lower';
+    }
+
+    // Armor Pen
+    if (guess.armorPenValue === correct.armorPenValue) {
+        result.armorPen = 'correct';
+    } else if (guess.armorPenValue > correct.armorPenValue) {
+        result.armorPen = 'higher';
+    } else {
+        result.armorPen = 'lower';
+    }
+
+    // Traits
+    if (allTraitsMatch) {
+        result.traits = 'correct';
+    } else if (hasSharedTrait) {
+        result.traits = 'partial';
+    } else {
+        result.traits = 'incorrect';
     }
 
     return result;
