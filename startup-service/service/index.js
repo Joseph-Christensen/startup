24d206faa044871c186ec7566a8b1768ef511997
Joseph-Christensen <josephchristensen06@gmail.com>
2025-11-02 22:37:41 -0700

    login endpoint finished

--- startup-service/service/index.js
+++ startup-service/service/index.js
@@ -44,19 +44,18 @@ apiRouter.post('/auth/create', async (req, res) => {
   }
 });
 
-// // GetAuth login an existing user
-// apiRouter.post('/auth/login', async (req, res) => {
-//   const user = await findUser('email', req.body.email);
-//   if (user) {
-//     if (await bcrypt.compare(req.body.password, user.password)) {
-//       user.token = uuid.v4();
-//       setAuthCookie(res, user.token);
-//       res.send({ email: user.email });
-//       return;
-//     }
-//   }
-//   res.status(401).send({ msg: 'Unauthorized' });
-// });
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const {username, password} = req.body;
+  const user = await findUser('username', username);
+  if (user && await bcrypt.compare(password, user.password)) {
+    user.token = uuid.v4();
+    setAuthCookie(res, user.token);
+    res.send({ username: user.username });
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
 
 // // DeleteAuth logout a user
 // apiRouter.delete('/auth/logout', async (req, res) => {
@@ -121,34 +120,34 @@ apiRouter.post('/auth/create', async (req, res) => {
 //   return scores;
 // }
 
-// async function createUser(email, password) {
-//   const passwordHash = await bcrypt.hash(password, 10);
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
 
-//   const user = {
-//     email: email,
-//     password: passwordHash,
-//     token: uuid.v4(),
-//   };
-//   users.push(user);
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
 
-//   return user;
-// }
+  return user;
+}
 
-// async function findUser(field, value) {
-//   if (!value) return null;
+async function findUser(field, value) {
+  if (!value) return null;
 
-//   return users.find((u) => u[field] === value);
-// }
+  return users.find((u) => u[field] === value);
+}
 
-// // setAuthCookie in the HTTP response
-// function setAuthCookie(res, authToken) {
-//   res.cookie(authCookieName, authToken, {
-//     maxAge: 1000 * 60 * 60 * 24 * 365,
-//     secure: true,
-//     httpOnly: true,
-//     sameSite: 'strict',
-//   });
-// }
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
