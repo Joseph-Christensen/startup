a0c3a448bea1511bf8b829ac7cd53ac0f4c1e0e2
Joseph-Christensen <josephchristensen06@gmail.com>
2025-11-11 16:54:28 -0700

    adding new collections in database and new functions for scores

--- startup-db/service/database.js
+++ startup-db/service/database.js
@@ -6,6 +6,10 @@ const client = new MongoClient(url);
 const db = client.db('diverdle');
 const userCollection = db.collection('users');
 const scoreCollection = db.collection('scores');
+const allScoreCollection = db.collection('allScores');
+const gameStateCollection = db.collection('gameStates');
+const dailyQuoteCollection = db.collection('dailyQuote');
+const dailyWeaponCollection = db.collection('dailyWeapon');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -18,20 +22,39 @@ const scoreCollection = db.collection('scores');
   }
 })();
 
+// returning user by username
 function getUser(username) {
   return userCollection.findOne({ username: username });
 }
 
+// returning user by token
 function getUserByToken(token) {
   return userCollection.findOne({ token: token });
 }
 
+// adding user to the db
 async function addUser(user) {
   await userCollection.insertOne(user);
 }
 
+// updating user info (auth)
 async function updateUser(user) {
   await userCollection.updateOne({ username: user.username }, { $set: user });
+}
+
+// adding score to db
+async function addScore(score) {
+  await scoreCollection.insertOne(score);
+}
+
+// returning scores from db
+async function getScores() {
+  return scoreCollection.find().toArray();
+}
+
+// clearing scores
+async function clearScores() {
+  await scoreCollection.deleteMany({});
 }
 
 module.exports = {
@@ -39,4 +62,7 @@ module.exports = {
     getUserByToken,
     addUser,
     updateUser,
+    addScore,
+    getScores,
+    clearScores,
 };
